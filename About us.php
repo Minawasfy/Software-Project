e302e05639b45a3798deb466c57f70bd22dd8a7b
Zyzzx201 <36744908+Zyzzx201@users.noreply.github.com>
2018-02-24 19:00:39 +0200

    Add files via upload

---
+++ About us.php
@@ -0,0 +1,38 @@
+<html>
+
+<head>
+<title>
+Fun & Learn
+</title>
+<h1>About Us</h1>
+
+</head>
+
+<body>
+
+<div class = "mission">
+
+Mission :
+
+
+</div>
+<br>
+<div class = "vision">
+
+<h3>Vision:</h3><strong>Learn:</strong> New skills & how to face future life challenges through our academic program which is very well prepared to help children enhancing their language skills, imagination, gross/fine motor skills, cognitive/thinking skills, social skills,and mental principles. <br>
+
+<strong>Fun:</strong> quality time, where children can enjoy being creative, being part of a team, solving problems, trying to win, accepting loss, share, work on motor skills,and act in live preformances.
+
+
+</div>
+
+<br>
+
+<div class = "footer">
+contact us : <br>1-#######<br>
+2-######## <br>
+our branches are in both mearag and zahraa el maadi
+</div>
+
+
+</html>
