8216ce4bc653ab421ca1fe78b05a63217562b691
Minawasfy <36741779+Minawasfy@users.noreply.github.com>
2018-02-26 22:55:33 +0200

    Add files via upload

---
+++ User Side/index.php
@@ -0,0 +1,75 @@
+<html>
+
+<head>
+<title>
+Fun & Learn: Welcome
+</title>
+<h1>Welcome</h1>
+
+</head>
+
+<body>
+
+<header>
+    
+<h1>User Side</h1>
+<br>
+<a href="/UserSide/About us.php" > About us </a>
+<br>
+<a href="/UserSide/Contactus.php" > Contact us </a>
+<br>
+<a href="/UserSide/Onlineapplication.php" > Online Application </a>
+<br>
+<a href="/UserSide/Schedules.php">Schedule</a>     
+<br>
+<a href="/UserSide/addteacher.php">Add Teacher</a>
+<br>
+    
+<h2>Admin Side</h2>
+<br>        
+<a href="/AdminSide/acceptteacher.php">Accept Teacher</a>     
+<br>
+<a href="/AdminSide/editteacher.php">Edit Teacher</a>     
+<br>
+<a href="/AdminSide/Addusers.php">Add User</a>     
+<br>
+<a href="/AdminSide/deleteuser.php">Delete User</a> 
+<br>
+
+    
+</header>    
+<div align="justify" class = "wt">
+<strong><h2>An important Message to all the parents : </h2></strong>
+<hr>
+Many parents are concerned when their children are not practicing letters 
+and numbers, they feel that completing paper and pencil exercises will most 
+effectively prepare their children for elementary school. To change this common idea
+in our society we are working in our nursery hand in hand with parents in order to 
+enrich your child's physical and mental development through our "Fun and Learn" strategy,
+where the child can enjoy learning through playing by using different activities.
+Our concept is aiming to increase the child's self confidence, encourage him to think
+widely and independently & work using his/her  own hands to prepare him to do pencil & paper
+work, a step that should come at some stage of preschool education without stressing over it.
+</div>
+<hr>
+<div align="justify" class="ei">
+
+<h2>Extra Information:</h2>
+<ul>
+<li>We have opened our second branch @ Al Me3rag, starting october 2017 </li>
+<li>We have the honor serving Maadi children since 2008 </li>
+<li>Ask about our new baby class for age under 1 year old </li>
+<li>Transportation is available inside Maadi</li>
+<li>All teachers are foreigners or egyptians who are fluent in English </li>
+</ul>
+</div>
+
+<!--Inlucde footer file-->
+
+<br>
+
+</body>
+
+
+
+</html>
