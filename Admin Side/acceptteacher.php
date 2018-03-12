558df6c5fefcac70384c1af0fb5927032e6bbd29
Daizy <36770670+daizy96@users.noreply.github.com>
2018-03-13 01:28:27 +0200

    oop php

--- Admin Side/acceptteacher.php
+++ Admin Side/acceptteacher.php
@@ -1,129 +1,33 @@
+<!DOCTYPE html>
 <html>
 <?php
   require 'db.php';
   require 'addteacher.php';
-
-  $Fname=$tdob=$nat=$address1=$Tell=$number1=$number2=$status=$q1="";
-  $q2=$q3=$q4=$q5=$q11=$q22=$q33=$q44=$q55=$Emname=$num123=$Emnum="";
-  $salary=$salary1=$other1=$other="";
-
-  $sql= "SELECT * FROM (/*-table name-*/)"; /*btgib el records mn el db*/
-
-  if ($_SERVER["REQUEST_METHOD"] == "REQUEST") {
-    $Fname=$_REQUEST['Fname'];
-    $tdob=$_REQUEST['tdob'];
-    $nat=$_REQUEST['nat'];
-    $address1=$_REQUEST['address1'];
-    $Tell=$_REQUEST['Tell'];
-    $number1=$_REQUEST['number1'];
-    $number2=$_REQUEST['number2'];
-    $status=$_REQUEST['status'];
-    $q1=$_REQUEST['q1'];
-    $q2=$_REQUEST['q2'];
-    $q3=$_REQUEST['q3'];
-    $q4=$_REQUEST['q4'];
-    $q5=$_REQUEST['q5'];
-    $q11=$_REQUEST['q11'];
-    $q22=$_REQUEST['q22'];
-    $q33=$_REQUEST['q33'];
-    $q44=$_REQUEST['q44'];
-    $q55=$_REQUEST['q55'];
-    $Emname=$_REQUEST['Emname'];
-    $Emnum=$_REQUEST['Emnum'];
-    $num123=$_REQUEST['num123']
-    $salary=$_REQUEST['salary'];
-    $salary1=$_REQUEST['salary1'];
-    $other1=$_REQUEST['other1'];
-    $other=$_REQUEST['other'];
+  $mysqli=new mysqli('localhost','root','','swe');
+  if(mysqli_connect_errno())
+  {
+   print_f("Connection Failed: %s\n",mysqli_connect_error());
   }
-
-  $result= $db->excute($sql);
-  $row = $result->fetch_assoc();
-  if ($result-> num_rows> 0){
-    $Fname=$row['Fname'];
-    $tdob=$row['tdob'];
-    $nat=$row['nat'];
-    $address1=$row['address1'];
-    $number1=$row['number1'];
-    $number2=$row['number2'];
-    $status=$row['status'];
-    $q1=$row['q1'];
-    $q2=$row['q2'];
-    $q3=$row['q3'];
-    $q4=$row['q4'];
-    $q5=$row['q5'];
-    $q11=$row['q11'];
-    $q22=$row['q22'];
-    $q33=$row['q33'];
-    $q44=$row['q44'];
-    $q55=$row['q55'];
-    $Emname=$row['Emname'];
-    $Emnum=$row['Emnum'];
-    $num123=$row['num123']
-    $salary=$row['salary'];
-    $salary1=$row['salary1'];
-    $other1=$row['other1'];
-    $other=$row['other'];
-    echo '<div class = "teacher2">
-      <form method="REQUEST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-        Full name: '.$Fname.'
-        <br><br>
-        Date of birth: '.$tdob.'
-        <br><br>
-        Nationality age: '.$nat.'
-        <br><br>
-        Home Address: '.$address1.'
-        <br><br>
-        Telephone number: '.$Tell.'
-        <br><br>
-        Mobile number 1: '.$number1.'
-        <br><br>
-        Mobile number 2: '.$number2.'
-        <br><br>
-        Marital Status: '.$status.' <br>
-        Academic Qualifications with Dates:<br>
-        Qualification 1: '.$q1.'
-        <br>
-        Qualification 2: '.$q2.'
-        <br>
-        Qualification 3: '.$q3.'
-        <br>
-        Qualification 4: '.$q4.'
-        <br>
-        Qualification 5: '.$q5.'
-        <br><br><hr>
-        Professional Qualifications with Dates:<br>
-        Qualification 1: '.$q11.'
-        <br>
-        Qualification 2: '.$q22.'
-        <br>
-        Qualification 3: '.$q33.'
-        <br>
-        Qualification 4: '.$q44.'
-        <br>
-        Qualification 5: '.$q55.'
-
-        Present Employer s Name: '.$Emname.'
-
-        Present Employer s Address: '.$num123.'
-        <br><br>
-        Present Employer s phone number: '.$Emnum.'
-        <br><br><hr>
-
-        Current or Last Salary: '.$salary.'
-        <br><br>
-        Required Salary: '.$salary1.'
-        <br><br>
-
-        Have you been interviewed recently at other nurseries? if yes, please mention names:<br> '.$other1.'
-        <br><br>
-
-        In your point of view, how do you see an ideal nursery regarding its academic side?<br> '.$other.'
-        <br>
-        <br><br>
-        </form></div>'
-      }
- ?>
+   class teacherACC
+   {
+     private $utype $Fname, $dob, $nat, $address1, $Tell, $mobile1, $number2, $maritalstatus, $q1;
+     private $q2, $q3, $q4, $q5, $q11, $q22, $q33, $q44, $q55, $prempname, $prempaddress, $prempnum;
+     private $corlsalary, $reqsalary, $othernursurey, $povnersurey;
+     var $db_obj = new dbconnect;
+     var $con = $db_obj.connect();
+     public static function display_teacher ()
+     {
+       $sql = "SELECT main.appstatus, main.fname, main.lname, nationality.name,
+         teacher.telnum, address.name, dob.day, dob.month, dob.year, teacher.mobile1, teacher.maritalstatus,
+         teacher.acaqual1, teacher.date_acaqual1, teacher.personal_qual1, teacher.date_ppersonalqual1, teacher.pempname,
+         teacher.pempnum, teacher.corlsalary, teacher.reqsalary, teacher.othernursery, teacher.povnursery
+         FROM main, teacher, nationality, address, dob WHERE main.id=teacher.main_id AND teacher.nationality=nationality.id
+         AND teacher.address_id= address.id AND main.dob_id= dob.id";
+         $result = $db_obj.executesql($sql);
+         $row = $result.fetch_assoc();
+     }
+   }
+   ?>
 <head>
   <title>Fun & Learn</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -131,7 +35,6 @@
   <link rel="stylesheet" href="AdminSS.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!--arrow down-->
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-  <h1 align="left" id="h11"> <p><u>Teacher Application:</u></p> </h1>
 </head>
 
 <body>
@@ -144,26 +47,41 @@
     </header>
 
     <div class = "teacher2">
-      <!--<form method="REQUEST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-        Full name:
-        <input type="text" name="Fname" id="boxes" required><br><br>
+      <form id="searchB" method="POST" action="<?php $_SERVER["PHP_SELF"];?>">
+           <input type="text" name="Search"id="boxes" placeholder="Search for names.." onkeyup="myFunction()">
+      </form>
+      <h1 align="left" id="h11"> <p><u>Teacher Application:</u></p> </h1>
+      <form method="REQUEST" action="<?php $_SERVER["PHP_SELF"];?>">
+        <?php
+        if($_SERVER["REQUEST_METHOD"]=="POST")
+        {
+          $s=$_POST["Search"];
+          if($s)
+          {
+            $sql2="SELECT main.appstatus, main.fname, main.lname, nationality.name,
+              teacher.telnum, address.name, dob.day, dob.month, dob.year, teacher.mobile1, teacher.maritalstatus,
+              teacher.acaqual1, teacher.date_acaqual1, teacher.personal_qual1, teacher.date_ppersonalqual1, teacher.pempname,
+              teacher.pempnum, teacher.corlsalary, teacher.reqsalary, teacher.othernursery, teacher.povnursery
+              FROM main, teacher, nationality, address, dob WHERE main.id=teacher.main_id AND teacher.nationality=nationality.id
+              AND teacher.address_id= address.id AND main.dob_id= dob.id AND main.fname Like '$s' AND main.lname Like '$s' ";
+            $array=array();
+            $result2=$db_obj2.executesql($sql2); elmoshkla mn awl hna ****
+            while($row2=mysqli_fetch_array($result2))
+            {
+                $result2=$db_obj2.executesql($sql2);
+                $row2 = mysqli_fetch_array($result2);
+                if(($s==$row2['fname'])||($s==$row2['lname'])):
+      ?>
+        Full name: <?php echo $row['fname']; $row['lname'];?><br><br>
         Date of birth:
-        <input type="Date" name="tdob" id="boxes" required><br><br>
-        Nationality age:
-        <input type="number" name="nat" id="boxes" required><br><br>
-        Home Address:
-        <input type="text" name="address1" id="boxes" required><br><br>
-        Telephone number:
-        <input type="text" name="Tel" maxlength="8" id="boxes" required><br><br>
-        Mobile number 1:
-        <input type="text" name="number1" maxlength="11" id="boxes" required><br><br>
+        <?php echo $row['dob'];?><br><br>
+        Nationality age:<?php echo $row['nat'];?><br><br>
+        Home Address: <?php echo $row['address1'];?><br><br>
+        Telephone number:<?php echo $row['telnum'];?><br><br>
+        Mobile number 1:<?php echo $row['mobile1'];?><br><br>
         Mobile number 2:
         <input type="text" name="number2" maxlength="11" id="boxes"><br><br>
-        Marital Status:<br>
-        <input type="radio" name="status" value="Single" checked>Single<br>
-        <input type="radio" name="status" value="Married">Married<br>
-        <input type="radio" name="status" value="Divorced">Divorced<br>
-        <input type="radio" name="status" value="Widowed">Widowed<br><br>
+        Marital Status: <?php echo $row['maritalstatus'];?><br><br>
         Academic Qualifications with Dates:<br>
         Qualification 1:
         <textarea rows="1" cols="50" id="boxes"></textarea>
@@ -197,42 +115,40 @@
         <textarea rows="1" cols="50" id="boxes"></textarea>
         <input type="Date" name="q55" id="boxes"><br><br><hr>
 
-        Present Employer's Name:
-        <input type="text" name="Emname" id="boxes" required><br><br>
+        Present Employer's Name: <?php echo $row['prempname'];?><br><br>
         Present Employer's Address:
         <input type="text" name="num123"  id="boxes"><br><br>
-        Present Employer's phone number:
-        <input type="text" name="Emnum" id="boxes" maxlength="11" required><br><br><hr>
+        Present Employer's phone number: <?php echo $row['prempnum'];?><br><br><hr>
 
-        Current or Last Salary:
-        <input type="number" name="salary" id="boxes" min="1" max ="10000" required><br><br>
-        Required Salary:
-        <input type="number" name="salary1" id="boxes" min="1" max ="10000" required><br><br>
+        Current or Last Salary: <?php echo $row['corlsalary'];?><br><br>
+        Required Salary: <?php echo $row['reqsalary'];?><br><br>
 
         Have you been interviewed recently at other nurseries? if yes, please mention names:<br>
-        <input type="text" name="other1" id="boxes"><br><br>
+        <?php echo $row['othernursery'];?><br><br>
 
         In your point of view, how do you see an ideal nursery regarding its academic side?<br>
-        <textarea rows="1" cols="50" name="other" id="boxes"></textarea><br>
-        <br><br>-->
+        <?php echo $row['povnursery'];?></textarea><br> <?php } } ?>
+        <br><br>
         <button type="button" id="ACbtn" name="ACbtn">Accept Applicant</button>
         <button type="button" id="RFbtn" name="RFbtn">Refuse Applicant</button>
         <?php
-          $ACbtn=$RFbtn=$appStatus"";
-          $sql="";
-          $appStatus=$_REQUEST['App Status']
-          $ACbtn= 'ACbtn';
-          $RFbtn= 'RFbtn';
-          if (isset($ACbtn)) {
-            $appStatus=0;
+          class AccBTNS
+          {
+            $ACbtn, $RFbtn, $appStatus, $sql;
+            $appStatus=$_REQUEST['App Status']
+            $ACbtn= 'ACbtn';
+            $RFbtn= 'RFbtn';
+            if (isset($ACbtn)) {
+              $appStatus=0;
+            }
+            elseif (isset($RFbtn)) {
+              $appStatus=1;
+            }
+            else {
+              $appStatus=2;
+            }
           }
-          elseif (isset($RFbtn)) {
-            $appStatus=1;
-          }
-          else {
-            $appStatus=2;
-          }
-         ?>
+        ?>
       </form>
     </div>
 
